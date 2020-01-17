abcdff8aa9ea76141adbac5732a2c108a5502b4a
Shun Lung Chen <slchen@mobisocial.us>
2020-01-17 22:37:56 +0800

    InMemoryFeedStore retrieve delivers empty on empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -5,6 +5,19 @@
 import XCTest
 import FeedStoreChallenge
 
+class InMemoryFeedStore: FeedStore {
+
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    }
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    }
+}
+
 class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	
 //
@@ -14,9 +27,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 //
 
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -88,7 +101,8 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	// - MARK: Helpers
 	
 	private func makeSUT() -> FeedStore {
-		fatalError("Must be implemented")
+		let sut = InMemoryFeedStore()
+        return sut
 	}
 	
 }
